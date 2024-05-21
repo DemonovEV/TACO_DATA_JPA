d0e997dd22165c592b4fb92287f667d35a33d1a3
Демонов Евгений Васильевич <edemonov@dev.vtb.ru>
2024-05-21 13:05:21 +0300

    ver2
    (cherry picked from commit 3ac68e5b2748a2dd4bcfed9d6ac47ac0f509f3f9)

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,7 +1,6 @@
 package tacos.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
