fc5c906233cfb9197b40389525805ec0b751be6b
Демонов Евгений Васильевич <edemonov@dev.vtb.ru>
2024-06-28 21:28:36 +0300

    Перевел на версию библиотек 3.2.3

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
 import tacos.data.OrderRepository;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/orders")
