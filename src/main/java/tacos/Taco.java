73318a0d59653ea6b9bad3cf92e348c4e42657c6
Демонов Евгений Васильевич <edemonov@dev.vtb.ru>
2024-10-17 15:28:02 +0300

    SYNC
    (cherry picked from commit fc678833204082fa20909e4bf6a3beec21d0909b)

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -22,12 +22,12 @@ public class Taco {
     @Id
     private Long id;
 
+    private Date createdAt = new Date();
+
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    private Date createdAt = new Date();
-    
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<IngredientRef> ingredients = new ArrayList<>();
     //private List<Ingredient> ingredients;
