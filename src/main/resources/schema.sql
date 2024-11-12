2e7eafc8c1d6ed9d773325e9055c2a27ecb354ad
Демонов Евгений Васильевич <edemonov@dev.vtb.ru>
2024-11-12 15:51:58 +0300

    Убрал промежуточную таблицу taco_order_tacos и переключил на счетчики
    (cherry picked from commit 4d1f876d1ea68a969c13435db28bf65c57d64fb8)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,11 @@
 drop table  if  exists INGREDIENT_COLLECTION;
 drop table  if  exists TACO_ORDER_TACOS;
-
 drop table  if  exists TACO;
 drop table  if  exists TACO_ORDER;
-drop table  if  exists INGREDIENT ;
+drop table  if  exists INGREDIENT;
+
+create sequence TACO_ORDER_SEQ  iNCREMENT BY 50;
+create sequence TACO_SEQ iNCREMENT BY 50;
 
 /* Formatted on 12/11/2024 12:01:04 (QP5 v5.360) */
 CREATE TABLE if not    exists       Taco_Order
@@ -24,15 +26,9 @@ CREATE TABLE if not       exists             Taco
 (
     id                identity /* PRIMARY KEY*/,
     created_at        TIMESTAMP NOT NULL,
-    name              VARCHAR (50) NOT NULL
+    name              VARCHAR (50) NOT NULL,
+    ref_to_taco_order    bigint NOT NULL REFERENCES Taco_Order (id) // Используется заз счет  @JoinColumn(name = "REF_TO_TACO_ORDER")
 );
-/* Formatted on 12/11/2024 14:53:05 (QP5 v5.360) */
-CREATE TABLE if not          exists         taco_order_tacos
-(
-    taco_order_id    bigint NOT NULL REFERENCES Taco_Order (id),
-    tacos_id         bigint NOT NULL references Taco(id)
-);
-
 
 /* Formatted on 12/11/2024 12:07:14 (QP5 v5.360) */
 CREATE TABLE if not    exists  Ingredient
@@ -48,3 +44,14 @@ CREATE TABLE if not    exists       ingredient_collection
     ref_to_ingredient   VARCHAR (4) NOT NULL REFERENCES Ingredient (id), // defaut ingredients_id
     ref_to_taco         bigint NOT NULL REFERENCES taco (id) //defaut taco_id
 );
+
+
+
+/* Formatted on 12/11/2024 14:53:05 (QP5 v5.360) */
+/*
+CREATE TABLE if not          exists         taco_order_tacos
+(
+    taco_order_id    bigint NOT NULL REFERENCES Taco_Order (id),
+    tacos_id         bigint NOT NULL references Taco(id)
+);
+*/
