f2a8b0cfbf1f971d2b2497423326d0b522c3ce1e
Демонов Евгений Васильевич <edemonov@dev.vtb.ru>
2024-11-12 15:19:23 +0300

    Прописал явно JoinTable для определения колонки ссылки и колонки order

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -21,8 +21,9 @@ public class TacoOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-// Todo @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MY_OWN_SEQ")
+   // @GeneratedValue(strategy = GenerationType.AUTO)
+// Todo
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MY_OWN_SEQ")
     private Long id;
 
     private Date placedAt = new Date();
@@ -53,7 +54,7 @@ public class TacoOrder implements Serializable {
     // @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL,targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,95 +1,50 @@
-create table if not exists Taco_Order
+drop table  if  exists INGREDIENT_COLLECTION;
+drop table  if  exists TACO_ORDER_TACOS;
+
+drop table  if  exists TACO;
+drop table  if  exists TACO_ORDER;
+drop table  if  exists INGREDIENT ;
+
+/* Formatted on 12/11/2024 12:01:04 (QP5 v5.360) */
+CREATE TABLE if not    exists       Taco_Order
 (
-    id
-    identity,
-    delivery_Name
-    varchar
+    id                 identity/* PRIMARY KEY*/,
+    cc_cvv             VARCHAR (3) NOT NULL,
+    cc_expiration      VARCHAR (5) NOT NULL,
+    cc_number          VARCHAR (16) NOT NULL,
+    delivery_City      VARCHAR (50) NOT NULL,
+    delivery_Name      VARCHAR (50) NOT NULL,
+    delivery_State     VARCHAR (2) NOT NULL,
+    delivery_Street    VARCHAR (50) NOT NULL,
+    delivery_Zip       VARCHAR (10) NOT NULL,
+    placed_at          TIMESTAMP NOT NULL
+);
+/* Formatted on 12/11/2024 12:22:10 (QP5 v5.360) */
+CREATE TABLE if not       exists             Taco
 (
-    50
-) not null,
-    delivery_Street varchar
+    id                identity /* PRIMARY KEY*/,
+    created_at        TIMESTAMP NOT NULL,
+    name              VARCHAR (50) NOT NULL
+);
+/* Formatted on 12/11/2024 14:53:05 (QP5 v5.360) */
+CREATE TABLE if not          exists         taco_order_tacos
 (
-    50
-) not null,
-    delivery_City varchar
-(
-    50
-) not null,
-    delivery_State varchar
-(
-    2
-) not null,
-    delivery_Zip varchar
-(
-    10
-) not null,
-    cc_number varchar
-(
-    16
-) not null,
-    cc_expiration varchar
-(
-    5
-) not null,
-    cc_cvv varchar
-(
-    3
-) not null,
-    placed_at timestamp not null
-    );
+    taco_order_id    bigint NOT NULL REFERENCES Taco_Order (id),
+    tacos_id         bigint NOT NULL references Taco(id)
+);
 
 
-create table if not exists Taco
+/* Formatted on 12/11/2024 12:07:14 (QP5 v5.360) */
+CREATE TABLE if not    exists  Ingredient
 (
-    id
-    identity,
-    name
-    varchar
+    id      VARCHAR (4) NOT NULL PRIMARY KEY ,
+    name    VARCHAR (25) NOT NULL,
+    TYPE    VARCHAR (10) NOT NULL
+);
+
+/* Formatted on 12/11/2024 12:30:51 (QP5 v5.360) */
+CREATE TABLE if not    exists       ingredient_collection
 (
-    50
-) not null,
-    taco_order bigint not null,
-    taco_order_key bigint not null,
-    created_at timestamp not null
-    );
-
-
-alter table Taco
-    add foreign key (taco_order) references Taco_Order (id);
-
-
-create table if not exists Ingredient
-(
-    id varchar
-(
-    4
-) not null,
-    name varchar
-(
-    25
-) not null,
-    type varchar
-(
-    10
-) not null
-    );
-alter table Ingredient add primary key (ID);
-
-alter table Ingredient
-    add foreign key (ingredient) references Ingredient (id);
-
-create table if not exists Ref_To_Ingredient
-(
-    ingredient varchar
-(
-    4
-) not null,
-    taco bigint not null,
-    taco_key bigint not null
-    );
-
-alter table Ref_To_Ingredient
-    add foreign key (ingredient) references Ingredient (id);
-
-alter table Ref_To_Ingredient
-    add foreign key (taco) references taco (id);
+    ref_to_ingredient   VARCHAR (4) NOT NULL REFERENCES Ingredient (id), // defaut ingredients_id
+    ref_to_taco         bigint NOT NULL REFERENCES taco (id) //defaut taco_id
+);
