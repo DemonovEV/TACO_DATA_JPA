5e92cde4db9f1c3d48afbbc634eba67127f96bd6
DemonovEV <edemonov@dev.vtb.ru>
2024-12-27 19:59:28 +0300

    Перевел на PostgreSQL
    (cherry picked from commit 12dc08ef0d41ee7d69c6999491eaec2f583d1520)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-drop table if exists INGREDIENT_COLLECTION;
-drop table if exists INGREDIENT;
-drop table if exists TACO;
-drop table if exists TACO_ORDER;
+drop table if exists ingredient_collection;
+drop table if exists ingredient;
+drop table if exists taco;
+drop table if exists taco_order;
 
 
 drop sequence if exists TACO_ORDER_SEQ;
@@ -11,9 +11,9 @@ drop sequence if exists TACO_SEQ;
 create sequence TACO_ORDER_SEQ iNCREMENT BY 50;
 create sequence TACO_SEQ iNCREMENT BY 50;
 
-CREATE TABLE if not exists Taco_Order
+CREATE TABLE if not exists taco_order
 (
-    id              identity/* PRIMARY KEY*/,
+    id              SERIAL PRIMARY KEY,
     cc_cvv          VARCHAR(3)  NOT NULL,
     cc_expiration   VARCHAR(5)  NOT NULL,
     cc_number       VARCHAR(16) NOT NULL,
@@ -25,15 +25,15 @@ CREATE TABLE if not exists Taco_Order
     placed_at       TIMESTAMP   NOT NULL
 );
 
-CREATE TABLE if not exists Taco
+CREATE TABLE if not exists taco
 (
-    id                identity /* PRIMARY KEY*/,
+    id                SERIAL PRIMARY KEY,
     created_at        TIMESTAMP   NOT NULL,
     name              VARCHAR(50) NOT NULL,
     ref_to_taco_order bigint      NOT NULL REFERENCES Taco_Order (id) /* Используется за счет  @JoinColumn(name = "REF_TO_TACO_ORDER")*/
 );
 
-CREATE TABLE if not exists Ingredient
+CREATE TABLE if not exists ingredient
 (
     id   VARCHAR(4)  NOT NULL PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
