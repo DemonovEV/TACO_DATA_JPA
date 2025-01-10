de0b519c151f19b0c61ed25def5525404a9b0693
DemonovEV <edemonov@dev.vtb.ru>
2025-01-10 11:42:41 +0300

    Revert "FORMAT1"
    This reverts commit c4376cca467beeb851715612c4f872a83c3c4ccf.

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Ingredient {
     @Id
-    private String id1;
+    private String id;
     private String name;
     @Enumerated(EnumType.STRING) //В отсутствие аннотации @Enumerated Hibernate будет сохранять порядковый
             //(ORDINAL) номер значения
