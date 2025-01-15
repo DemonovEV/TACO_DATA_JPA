4af49bb63415124cda699b255016f29cbd3d730a
DemonovEV <edemonov@dev.vtb.ru>
2025-01-15 17:31:32 +0300

    Работает спринговый LOGIN

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,14 +2,19 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import tacos.data.UserRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -18,22 +23,22 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    /*
-       @Bean
-       public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-           List<UserDetails> usersList = new ArrayList<>();
-           usersList.add(new User(
-                   "buzz", encoder.encode("password"),
-                   Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-           usersList.add(new User(
-                   "woody", encoder.encode("password"),
-                   Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-           return new InMemoryUserDetailsManager(usersList);
-       }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+        usersList.add(new User(
+                "buzz", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User(
+                "woody", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(usersList);
+    }
 
 
-       /
-        */
+
+       /*
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo) {
         return username -> {
@@ -42,6 +47,8 @@ public class SecurityConfig {
             throw new UsernameNotFoundException("User ‘" + username + "’ not found");
         };
     }
+     */
+
     /*
     @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -59,6 +66,7 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                 {
@@ -69,9 +77,17 @@ public class SecurityConfig {
                             .permitAll();
                 });
 
-        http
-                .formLogin(Customizer.withDefaults());
 
+        http
+                .formLogin(httpSecurityFormLoginConfigurer ->
+                        httpSecurityFormLoginConfigurer
+                                .loginProcessingUrl("/authenticate")
+                                .usernameParameter("user")
+                                .passwordParameter("pwd")
+                                .defaultSuccessUrl("/design")
+                                .failureUrl("/login?error123")
+                );
+        //    5.3.3 Использование сторонних систем аутентификации
         return http.build();
     }
 }
