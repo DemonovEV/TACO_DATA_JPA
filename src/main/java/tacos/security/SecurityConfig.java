6c408cc96480239b26138d0f647c3734dab6ee55
DemonovEV <edemonov@dev.vtb.ru>
2025-01-17 11:44:16 +0300

    Переключил на userRepo

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -3,19 +3,12 @@ package tacos.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import tacos.data.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -24,7 +17,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
+ /*
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
         List<UserDetails> usersList = new ArrayList<>();
@@ -38,8 +31,8 @@ public class SecurityConfig {
     }
 
 
+ */
 
-       /*
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo) {
         return username -> {
@@ -48,7 +41,7 @@ public class SecurityConfig {
             throw new UsernameNotFoundException("User ‘" + username + "’ not found");
         };
     }
-     */
+
 
     /*
     @Bean
@@ -82,7 +75,7 @@ public class SecurityConfig {
         http
                 .formLogin(httpSecurityFormLoginConfigurer ->
                         httpSecurityFormLoginConfigurer
-                                   .loginPage("/mylogin")
+                                .loginPage("/mylogin")
                                 // .loginProcessingUrl("/authenticate")
                                 .usernameParameter("user")
                                 .passwordParameter("pwd")
@@ -92,12 +85,13 @@ public class SecurityConfig {
 
         http
                 .logout(httpSecurityLogoutConfigurer ->
-                        httpSecurityLogoutConfigurer.logoutSuccessUrl("/orders")
+                        httpSecurityLogoutConfigurer.logoutSuccessUrl("/")
                 );
-      /*  http
+        http
                 .csrf(
-                        AbstractHttpConfigurer::disable
-                );*/
+                        httpSecurityCsrfConfigurer ->
+                                httpSecurityCsrfConfigurer.ignoringRequestMatchers("/h2-console/**")
+                );
 
 
         return http.build();
