67c246b3835da46491a4ddb84639b328dd9e5c1f
DemonovEV <edemonov@dev.vtb.ru>
2025-02-24 15:16:13 +0300

    USERS

--- src/main/java/tacos/User.java
+++ src/main/java/tacos/User.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Arrays;
 import java.util.Collection;
 
-@Entity
+@Entity(name = "USERS")
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @RequiredArgsConstructor
