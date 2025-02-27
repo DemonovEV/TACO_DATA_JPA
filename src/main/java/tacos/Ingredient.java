95bbaa9d3fd0284e4cda5d9f6499313b9e3683d8
DemonovEV <edemonov@dev.vtb.ru>
2025-02-27 16:37:33 +0300

    fix3

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -4,21 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Ingredient {
     @Id
     private String id;
     private String name;
     @Enumerated(EnumType.STRING) //В отсутствие аннотации @Enumerated Hibernate будет сохранять порядковый
-            //(ORDINAL) номер значения
+    //(ORDINAL) номер значения
     private Type type;
 
     @Getter
