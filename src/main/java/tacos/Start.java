bfd6edd465b524186c2efd0b635a5798fc234768
DemonovEV <edemonov@dev.vtb.ru>
2025-04-21 19:08:40 +0300

    Вернул hibernate.properties

--- src/main/java/tacos/Start.java
+++ src/main/java/tacos/Start.java
@@ -1,12 +1,9 @@
 package tacos;
 
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import java.util.Properties;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Start {
@@ -14,34 +11,17 @@ public class Start {
 
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
-        System.out.println(
-                Student.class.isAssignableFrom(Student.class)
-        );
 
-        Properties properties = new Properties();
-        properties.put(Environment.DRIVER, "org.postgresql.Driver");
-        properties.put(Environment.URL, "jdbc:postgresql://localhost:5436/gele_main?currentSchema=taco_step4");
-        properties.put(Environment.USER, "gele_usr");
-        properties.put(Environment.PASS, "gele_usr");
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
-        properties.put(Environment.SHOW_SQL, "true");
-        properties.put(Environment.HBM2DDL_AUTO, "create");
-
-
-        var session=
-         new Configuration()
-               // .addProperties(properties)
-                 .addAnnotatedClass(Student.class)
-                 .configure()
-                 .buildSessionFactory().openSession();
+        var session =
+                new Configuration()
+                        .addAnnotatedClass(Student.class)
+                        .buildSessionFactory().openSession();
         System.out.println(session);
-        var fff=session.find(Student.class,2);
-        System.out.println(fff);
-       var transaction = session.beginTransaction();
-        var obj=new Student().setFirstName("as");
-        fff.setFirstName("HORRR");
-        session.save(fff);
-       transaction.commit();
+        System.out.println(session.find(Student.class, 2));
+        var ss = session.find(Student.class, 2);
+
+
+        //s.save(new Ingredient("ONE","TWO", Ingredient.Type.VEGGIES));
     }
 
 }
